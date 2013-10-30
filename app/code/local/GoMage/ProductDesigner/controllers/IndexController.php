dac56b27fc5a483d32fa8273f9343c496a56541c
Roman Bublik <bublik.roman@gmail.com>
2013-10-30 17:34:02 +0400

    change title for designer page

--- app/code/local/GoMage/ProductDesigner/controllers/IndexController.php
+++ app/code/local/GoMage/ProductDesigner/controllers/IndexController.php
@@ -49,6 +49,7 @@ class GoMage_ProductDesigner_IndexController extends Mage_Core_Controller_Front_
             $this->_redirectReferer();
         }
         $this->loadLayout();
+        $this->getLayout()->getBlock('head')->setTitle(Mage::getStoreConfig('design/head/default_title'));
         $this->renderLayout();
     }
 
