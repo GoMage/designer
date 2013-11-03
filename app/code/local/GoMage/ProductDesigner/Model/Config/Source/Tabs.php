9b5590e00ed3bab8f025110c360a9b90ed9d3f07
Roman Bublik <bublik.roman@gmail.com>
2013-11-03 15:30:48 +0400

    add active tab option

---
+++ app/code/local/GoMage/ProductDesigner/Model/Config/Source/Tabs.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * GoMage.com extension for Magento
+ *
+ * Long description of this file (if any...)
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * 
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the GoMage ProductDesigner module to newer versions in the future.
+ * If you wish to customize the GoMage ProductDesigner module for your needs
+ * please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   GoMage
+ * @package    GoMage_ProductDesigner
+ * @copyright  Copyright (C) 2013 GoMage.com (http://www.gomage.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Short description of the class
+ *
+ * Long description of the class (if any...)
+ *
+ * @category   GoMage
+ * @package    GoMage_ProductDesigner
+ * @subpackage Model
+ * @author     Roman Bublik <rb@gomage.com>
+ */
+class GoMage_ProductDesigner_Model_Config_Source_Tabs
+{
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => 'navigation', 'label' => Mage::helper('designer')->__('Choose Product')),
+            array('value' => 'design', 'label' => Mage::helper('designer')->__('Add Dessign')),
+            array('value' => 'text', 'label' => Mage::helper('designer')->__('Add Text')),
+            array('value' => 'upload_image', 'label' => Mage::helper('designer')->__('Upload Images'))
+        );
+    }
+}
