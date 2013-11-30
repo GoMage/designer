0b894dfb07a0402bc7dbbab4c1cab1c7c3a935c9
Roman Bublik <bublik.roman@gmail.com>
2013-11-30 14:09:07 +0400

    fonts sort order

--- app/code/local/GoMage/ProductDesigner/Model/Config/Source/Font.php
+++ app/code/local/GoMage/ProductDesigner/Model/Config/Source/Font.php
@@ -6,9 +6,9 @@ class GoMage_ProductDesigner_Model_Config_Source_Font
     {
       return array(
         array('value' => 'Arial', 'label' => 'Arial'),
-        array('value' => 'Times New Roman', 'label' => 'Times New Roman'),
         array('value' => 'Comic Sans MS', 'label' => 'Comic Sans'),
-        array('value' => 'Impact', 'label' => 'Impact')
+        array('value' => 'Impact', 'label' => 'Impact'),
+        array('value' => 'Times New Roman', 'label' => 'Times New Roman'),
       );
     }
 }
