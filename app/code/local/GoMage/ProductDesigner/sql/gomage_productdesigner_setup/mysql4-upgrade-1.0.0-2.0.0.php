930a61ed345140deeb7cfcc57ae4b58557248036
Andrey Zac <az@stepcart.com>
2015-04-21 11:57:54 +0300

    gomage-8652

--- app/code/local/GoMage/ProductDesigner/sql/gomage_productdesigner_setup/mysql4-upgrade-1.0.0-2.0.0.php
+++ app/code/local/GoMage/ProductDesigner/sql/gomage_productdesigner_setup/mysql4-upgrade-1.0.0-2.0.0.php
@@ -7,8 +7,8 @@
  * @author       GoMage
  * @license      http://www.gomage.com/license-agreement/  Single domain license
  * @terms of use http://www.gomage.com/terms-of-use/
- * @version      Release: 1.1.0
- * @since        Available since Release 1.1.0
+ * @version      Release: 2.0.0
+ * @since        Available since Release 2.0.0
  */
 
 $installer = $this;
